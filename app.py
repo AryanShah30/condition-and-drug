2590b3422cdbc2f18f5f4539e9a1b72c63d908e2
Aryan Shah <115439830+AryanShah30@users.noreply.github.com>
2024-06-29 21:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import subprocess
+
+def install_dependencies():
+    subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
+
+install_dependencies()
+
 import pickle
 import streamlit as st
 import pandas as pd
